2e3827ece93fa7546fc1e6e2c927d4edd57e8ee3
Adarsh-2296 <adarshadu4711@gmail.com>
2026-03-26 18:05:43 +0530

    [ADD] 26/03/2026 Initial Commit for machine management module

---
+++ Machine_Management/models/machine_transfer.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from odoo import fields,models,api,_
+from odoo.addons.test_convert.tests.test_env import record
+from odoo.orm.types import ValuesType
+
+
+class MachineTransfer(models.Model):
+    _name = 'machine.transfer'
+    _description = 'Machine Transfer'
+    _rec_name = 'sequence_no_transfer'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+
+    sequence_no_transfer = fields.Char(readonly=True, tracking=True,copy=False)
+    machine_id = fields.Many2one('machine.management',string="Machine",tracking=True,ondelete='restrict')
+    alternate_ids = fields.Many2many('machine.management',compute='_compute_alternate_ids')
+    serial_no = fields.Char(string="Serial No.",readonly=True, store=True,related="machine_id.serial_no",tracking=True)
+    transfer_date = fields.Date(required=True, string="Transfer Date",tracking=True,default=fields.Date.today())
+    transfer_type = fields.Selection(selection=[('install','Install'),('remove','Remove')],default='install',tracking=True)
+    partner_id = fields.Many2one('res.partner',string="Customer",tracking=True)
+    internal_notes = fields.Text(string="Internal Notes",tracking=True)
+    status = fields.Selection([('draft','Draft'),('done','Done')],default='draft',tracking=True)
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        print('vals_list')
+        """Create sequence number for transfer"""
+        for vals in vals_list:
+            if vals.get('sequence_no_transfer', _('New')) == _('New'):
+                vals['sequence_no_transfer'] = (self.env['ir.sequence'].next_by_code('machine.transfer'))
+
+        return super().create(vals_list)
+
+    @api.depends('transfer_type')
+    def _compute_alternate_ids(self):
+        """To set the domain of machine_id field according to the transfer type"""
+        for record in self:
+            if record.transfer_type == 'install':
+                record.alternate_ids = record.machine_id.search([('state','=','active')])
+            elif record.transfer_type == 'remove':
+                record.alternate_ids = record.machine_id.search([('state','=','in_service')])
+            else:
+                record.alternate_ids = record.machine_id.search([])
+
+    def transfer_machine_button(self):
+        """Button to confirm the transfer"""
+        self.machine_id.write({'partner_id': self.partner_id,'state': 'in_service'})
+        self.write({'status': 'done'})
---
+++ Machine_Management/models/res_partner.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import typing
+
+from odoo import fields,models,api
+from odoo.orm.types import ValuesType
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    machine_ids = fields.One2many('machine.management','partner_id')
+    machine_count = fields.Integer(compute='_compute_machine_count')
+
+    @api.depends()
+    def _compute_machine_count(self):
+        """To compute machine count of a partner"""
+        for record in self:
+            record.machine_count = len(record.machine_ids)
+
+    def action_machine_smart_button(self):
+        """list of machines of a customer"""
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'machines',
+            'view_mode': 'list,form',
+            'res_model': 'machine.management',
+            'domain': [('partner_id', '=', self.id)],
+            'context': "{'create': False}"
+        }
+
+    def action_archive(self):
+        """Archive the machines if customer is Archived"""
+        self.machine_ids.action_archive()
+        super().action_archive()
+
+    def action_unarchive(self):
+        """Unarchive the machines if customer is unarchived"""
+        inactive_macs = self.machine_ids.search([('active', '=', False),('partner_id.id', '=', self.id)])
+        inactive_macs.action_unarchive()
+        return super().action_unarchive()
+
+
+
+
