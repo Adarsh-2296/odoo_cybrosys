0000a608d391f22cb5e8f4abcb654dfa595259b9
Adarsh-2296 <adarshadu4711@gmail.com>
2026-04-04 16:55:54 +0530

    [ADD] 04\04\2026 Adding new module Milestone Tasks

---
+++ milestone_tasks/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Milestone Tasks',
+    'summary': """Create milestone and tasks""",
+    'description': """Create milestone and tasks""",
+    'version': "19.0.1.0",
+    'category': 'Sales',
+    'author': "Adarsh",
+    'website': "www.cybrosys.com",
+    'license': "LGPL-3",
+    'sequence':-6,
+    'installable': True,
+    'application': True,
+    'auto_install': True,
+    'depends': ['sale_management'],
+    'data': [
+        'views/sale_order_line_views.xml'
+    ]
+}
