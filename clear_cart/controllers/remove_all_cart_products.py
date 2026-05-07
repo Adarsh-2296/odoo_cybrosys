b794af4aaf9b24beef292f9190af1cd4a0648c42
Adarsh-2296 <adarshadu4711@gmail.com>
2026-05-07 17:28:22 +0530

    [ADD] 07\05\2026 Adding a new module pos rating

--- clear_cart/controllers/remove_all_cart_products.py
+++ clear_cart/controllers/remove_all_cart_products.py
@@ -1,12 +1,13 @@
 from odoo import http
 from odoo.http import request
+from werkzeug.utils import redirect
+
 
 class ClearCart(http.Controller):
     @http.route(['/shop/cart/removeall'], type='http',auth="public", website=True)
     def clear_cart_products(self, **kw):
-        partner_id = self.env.user.partner_id
-        orders = request.website._order
+        """Function to clear all products in the cart on button click"""
+        orders = request.cart
         print(orders)
-        order = self.env['sale.order'].sudo().search([('partner_id.id','=',partner_id),('website_id','!=',False,)])
-        order.sudo().unlink()
-        return request.render("website_sale.cart")
+        orders.sudo().unlink()
+        return redirect("/shop/cart")
---
+++ pos_rating/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'POS Rating',
+    'version': "19.0.1.0",
+    'summary': """Adds a rating field in the product form and displays in the pos""",
+    'description': """Adds a rating field in the product form and displays in the pos""",
+    'category': 'Point of Sale',
+    'author': "Adarsh",
+    'website': "www.cybrosys.com",
+    'license': "LGPL-3",
+    'sequence':-5,
+    'installable': True,
+    'application': True,
+    'auto_install': True,
+    'depends': ['point_of_sale','base'],
+    'data': [
+        'views/product_product_views.xml',
+    ],
+'assets': {
+        'point_of_sale._assets_pos': [
+            'pos_rating/static/src/**/*',
+        ],
+},
+}
