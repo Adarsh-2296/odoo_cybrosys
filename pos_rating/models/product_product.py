72e0a17460f0f2ae62176d7d520987d390f3a83c
Adarsh-2296 <adarshadu4711@gmail.com>
2026-05-11 17:34:34 +0530

    [UPT] 11\05\2026 updating the module pos rating

--- pos_rating/models/product_product.py
+++ pos_rating/models/product_product.py
@@ -4,7 +4,16 @@ from odoo import fields,models,api
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
-    pos_rating = fields.Selection([('1','1'),('2','2'),('3','3'),('4','4'),('5','5')],default='1',string="Pos Rating")
+    product_pos_rating = fields.Selection([('1','1'),('2','2'),('3','3'),('4','4'),('5','5')],default='1',string="Pos Rating")
+
+    @api.model
+    def _load_pos_data_fields(self, config_id):
+        """
+        Adds the 'pos_rating' field to the list of fields loaded into the POS.
+        """
+        data = super()._load_pos_data_fields(config_id)
+        data += ['product_pos_rating']
+        return data
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
