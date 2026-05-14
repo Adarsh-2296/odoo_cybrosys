1929f04e72090b6adce9ceadad8b7fdbeb22bbd8
Adarsh-2296 <adarshadu4711@gmail.com>
2026-05-14 16:13:17 +0530

    [UPT] 14\05\2026 updating the module pos rating

--- pos_rating/static/src/core/popup_pos/popup_pos.js
+++ pos_rating/static/src/core/popup_pos/popup_pos.js
@@ -1,52 +1,40 @@
 /** @odoo-module */
 import { Dialog } from "@web/core/dialog/dialog";
-import { Component } from "@odoo/owl";
+import { Component, useState} from "@odoo/owl";
 import { usePos } from "@point_of_sale/app/hooks/pos_hook";
-import { Dropdown } from "@web/core/dropdown/dropdown";
-import { DropdownItem } from "@web/core/dropdown/dropdown_item";
 import { AlertDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
 import { _t } from "@web/core/l10n/translation";
+import { redirect } from "@web/core/utils/urls";
+import { Input } from "@point_of_sale/app/components/inputs/input/input";
 
 export class PopupPos extends Component {
     static template = "pos_rating.PopupPos";
-    static components = { Dialog, Dropdown, DropdownItem };
+    static components = { Dialog, Input };
 
     setup() {
         this.pos = usePos();
-        this.payment = 0;
-    }
-
-    async forcepay(){
-        if (this.pos.user._role == 'manager'){
-            this.payment = 1;
-        }
-        else {
-            this.payment = 2;
-        }
-
-    }
-    async cancel(){
-        this.payment = 0;
+        this.state = useState({
+            selectedvalue : String,
+        });
     }
     async confirm() {
-        if(this.payment == 1){
-            this.pos.pay();
-            console.log(this.pos.pay);
-
-        }
-        if (this.payment == 2){
-            this.pos.env.services.dialog.add(AlertDialog, {
+        if(this.state.selectedvalue == 'Force Payment') {
+            if (this.pos.user._role == 'manager') {
+                var path = this.pos.router.path
+                redirect('/pos/ui/' + path.slice(8, 10) + 'payment' + path.slice(17))
+            }
+            else {
+                this.pos.env.services.dialog.add(AlertDialog, {
                     title: _t("Alert"),
                     body: _t(
                         "You don't have permission for this action!" +
                         "Contact your administrator."
                     ),
-            });
+                });
+            }
         }
-        if(this.payment == 0){
-            console.log(this.pos.user._role)
+        else{
             this.props.close();
-        }
-
     }
-}
+    }
+    }
