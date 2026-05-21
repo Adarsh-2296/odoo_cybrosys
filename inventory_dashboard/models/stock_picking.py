f95d5fc9a2bb600e86a182827de52d0294fdeb07
Adarsh-2296 <adarshadu4711@gmail.com>
2026-05-21 17:30:52 +0530

    [UPT] 21\05\2026 updating the module inventory_dashboard

--- inventory_dashboard/models/stock_picking.py
+++ inventory_dashboard/models/stock_picking.py
@@ -5,5 +5,103 @@ class StockPicking(models.Model):
    @api.model
    def get_tiles_data(self):
        company_id = self.env.company
-       result = self.search([('company_id', '=', company_id)]).mapped('name')
-       return {'result': result}
+       stock = self.search([('company_id', '=', company_id)])
+
+       """Location wise"""
+       location_id = self.env['stock.location'].search([('company_id', '=', company_id)])
+       location = {}
+       for i in location_id:
+           value = i.mapped('quant_ids.product_id.name')
+           location.update({i.name: len(value)})
+       location_name = list(location.keys())
+       location_product = list(location.values())
+
+       """Group based on picking type"""
+       picking_type_id = self.env['stock.picking.type'].search([('company_id', '=', company_id)])
+       picking_type_dict = {}
+       print(picking_type_id)
+       for i in picking_type_id:
+           value = stock.filtered(lambda j: j.picking_type_id.id == i.id)
+           picking_type_dict.update({i.warehouse_id.name + ' : ' + i.name : len(value)})
+       picking_type_name = list(picking_type_dict.keys())
+       picking_type_count = list(picking_type_dict.values())
+       print(picking_type_name)
+       print(picking_type_count)
+
+       """incoming and outgoing"""
+       incoming = stock.filtered(lambda i: i.picking_type_code == "incoming")
+       outgoing = stock.filtered(lambda i: i.picking_type_code == "outgoing")
+       incoming_dict = {}
+       outgoing_dict = {}
+       for i in incoming.move_ids:
+           if i.product_id.name not in incoming_dict:
+               incoming_dict.update({i.product_id.name : i.product_uom_qty })
+           else:
+               qty = incoming_dict[i.product_id.name]
+               incoming_dict.update({i.product_id.name: i.product_uom_qty + qty})
+       for i in outgoing.move_ids:
+           if i.product_id.name not in outgoing_dict:
+               outgoing_dict.update({i.product_id.name : i.product_uom_qty })
+           else:
+               qty = outgoing_dict[i.product_id.name]
+               outgoing_dict.update({i.product_id.name: i.product_uom_qty + qty})
+       outgoing_product = list(outgoing_dict.keys())
+       outgoing_qty = list(outgoing_dict.values())
+       incoming_product = list(incoming_dict.keys())
+       incoming_qty = list(incoming_dict.values())
+
+       """Internal transfers (Product wise)"""
+       internal = picking_type_id.filtered(lambda rec: rec.code == "internal")
+       internal_transfers = stock.filtered(lambda rec: rec.picking_type_id in internal)
+       internal_transfers_dict = {}
+       for i in internal_transfers.move_ids:
+           if i.product_id.name not in internal_transfers_dict:
+               internal_transfers_dict.update({i.product_id.name : i.product_uom_qty })
+           else:
+               qty = internal_transfers_dict[i.product_id.name]
+               internal_transfers_dict.update({i.product_id.name: i.product_uom_qty + qty})
+       internal_transfers_product = list(internal_transfers_dict.keys())
+       internal_transfers_qty = list(internal_transfers_dict.values())
+
+       """Warehouse and its location"""
+       warehouse_id = self.env['stock.warehouse'].search([('company_id', '=', company_id)])
+       warehouse_dict = {}
+       for i in warehouse_id:
+           value = location_id.filtered(lambda rec: rec.warehouse_id.id == i.id).mapped('name')
+           warehouse_dict.update({i.name : len(value)})
+       warehouse_name = list(warehouse_dict.keys())
+       warehouse_location = list(warehouse_dict.values())
+
+       """Average expense of  a product(purchase cost + landed cost)"""
+       products = self.env['product.product'].search([('is_storable', '=', True)])
+       products_dict = {}
+       for i in products:
+           products_dict.update({ i.name : i.avg_cost })
+       product_names = list(products_dict.keys())
+       product_avg_cost = list(products_dict.values())
+
+       """Inventory valuation"""
+       products_dict = {}
+       for i in products:
+           products_dict.update({i.name: i.total_value})
+       product_val_names = list(products_dict.keys())
+       product_val_avg_cost = list(products_dict.values())
+
+       return {
+                'outgoing_product' : outgoing_product,
+                'outgoing_qty' : outgoing_qty,
+                'incoming_product' : incoming_product,
+                'incoming_qty' : incoming_qty,
+                'location_name' : location_name,
+                'location_product' : location_product,
+                'picking_type_name' : picking_type_name,
+                'picking_type_count' : picking_type_count,
+                'internal_transfers_product' : internal_transfers_product,
+                'internal_transfers_qty' : internal_transfers_qty,
+                'warehouse_name' : warehouse_name,
+                'warehouse_location' : warehouse_location,
+                'product_names' : product_names,
+                'product_avg_cost' : product_avg_cost,
+                'product_val_names' : product_val_names,
+                'product_val_avg_cost' : product_val_avg_cost,
+               }
--- inventory_dashboard/static/src/js/dashboard.js
+++ inventory_dashboard/static/src/js/dashboard.js
@@ -2,6 +2,7 @@
 import { registry } from "@web/core/registry";
 import { Component } from "@odoo/owl";
 import { useService } from "@web/core/utils/hooks";
+import { loadJS } from "@web/core/assets";
 
 const actionRegistry = registry.category("actions");
 class InventoryDashboard extends Component {
@@ -12,10 +13,239 @@ class InventoryDashboard extends Component {
   }
   async _fetch_data(){
         let result = await this.orm.call("stock.picking", "get_tiles_data", [], {});
-        // document.getElementById('inventory_data').innerHTML = `<t t-foreach="${result}.result" t-as="re" t-key="re" t-out="re"></t>`;
-      for (var i =0;i<result.result.length;i++) {
-          document.getElementById('inventory_data').innerHTML += `<tr>${result.result[i]}</tr>`;
-      }
+        await  loadJS(["/web/static/lib/Chart/Chart.js"]);
+        var incoming = document.getElementById('incoming_data').getContext('2d');
+        var myChart = new Chart(incoming, {
+            type: 'line',
+            data: {
+                labels: result.incoming_product,
+                datasets: [{
+                    label: 'Quantity',
+                    data: result.incoming_qty,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                   beginAtZero: true
+               }
+           }
+       }
+   });
+        var outgoing = document.getElementById('outgoing_data').getContext('2d');
+        var myChart = new Chart(outgoing, {
+            type: 'doughnut',
+            data: {
+                labels: result.outgoing_product,
+                datasets: [{
+                    label: 'Quantity',
+                    data: result.outgoing_qty,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                   beginAtZero: true
+               }
+           }
+       }
+   });
+        var outgoing = document.getElementById('location_wise').getContext('2d');
+        var myChart = new Chart(outgoing, {
+            type: 'pie',
+            data: {
+                labels: result.location_name,
+                datasets: [{
+                    label: 'Count',
+                    data: result.location_product,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                   beginAtZero: true
+               }
+           }
+       }
+   });
+        var outgoing = document.getElementById('group_based').getContext('2d');
+        var myChart = new Chart(outgoing, {
+            type: 'pie',
+            data: {
+                labels: result.picking_type_name,
+                datasets: [{
+                    label: 'Count',
+                    data: result.picking_type_count,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                   beginAtZero: true
+               }
+           }
+       }
+   });
+        var outgoing = document.getElementById('internal_transfers').getContext('2d');
+        var myChart = new Chart(outgoing, {
+            type: 'bar',
+            data: {
+                labels: result.internal_transfers_product,
+                datasets: [{
+                    label: 'Count',
+                    data: result.internal_transfers_qty,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                   beginAtZero: true
+               }
+           }
+       }
+   });
+        var outgoing = document.getElementById('warehouse_location').getContext('2d');
+        var myChart = new Chart(outgoing, {
+            type: 'bar',
+            data: {
+                labels: result.warehouse_name,
+                datasets: [{
+                    label: 'Count',
+                    data: result.warehouse_location,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                   beginAtZero: true
+               }
+           }
+       }
+   });
+         var outgoing = document.getElementById('avg_expense').getContext('2d');
+        var myChart = new Chart(outgoing, {
+            type: 'line',
+            data: {
+                labels: result.product_names,
+                datasets: [{
+                    label: 'Count',
+                    data: result.product_avg_cost,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                   beginAtZero: true
+               }
+           }
+       }
+   });
+         var outgoing = document.getElementById('inventory_valuation').getContext('2d');
+        var myChart = new Chart(outgoing, {
+            type: 'doughnut',
+            data: {
+                labels: result.product_val_names,
+                datasets: [{
+                    label: 'Count',
+                    data: result.product_val_avg_cost,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                   beginAtZero: true
+               }
+           }
+       }
+   });
     }
 }
 InventoryDashboard.template = "inventory_dashboard.InventoryDashboard";
