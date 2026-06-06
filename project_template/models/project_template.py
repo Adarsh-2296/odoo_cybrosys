0c62ca8925f073eeac810378048ca4b8bd6cada0
Adarsh-2296 <adarshadu4711@gmail.com>
2026-06-06 17:12:53 +0530

    [UPT] 06\06\2026 updating the module project_template

--- project_template/models/project_template.py
+++ project_template/models/project_template.py
@@ -55,19 +55,63 @@ class ProjectTemplate(models.Model):
             'type_ids': [Command.link(stage.id) for stage in self.type_ids],
             'is_favorite': self.is_favorite,
         })
+        # if self.task_ids:
+        #     tasks = self.task_ids.filtered(lambda task: not task.parent_id)
+        #     for i in tasks:
+        #         self.env['project.task'].create({"name": i.name,
+        #                                                        'task_template_id': i.id,
+        #                                                        'user_ids': [Command.link(user.id) for user in i.user_ids],
+        #                                                        'project_id': project.id,
+        #                                                        'partner_id': i.partner_id.id,
+        #                                                        'date_deadline': i.date_deadline,
+        #                                                        'priority': i.priority,
+        #                                                        'description': i.description,
+        #                                                        'child_ids': [Command.create(task.name) for task in i.child_ids],
+        #                                                        })
         if self.task_ids:
-            tasks = self.task_ids.filtered(lambda task: not task.parent_id)
-            for i in tasks:
-                self.env['project.task'].create({"name": i.name,
-                                                               'task_template_id': i.id,
-                                                               'user_ids': [Command.link(user.id) for user in i.user_ids],
-                                                               'project_id': project.id,
-                                                               'partner_id': i.partner_id.id,
-                                                               'date_deadline': i.date_deadline,
-                                                               'priority': i.priority,
-                                                               'description': i.description,
-                                                               'child_ids': [Command.create(task.name) for task in i.child_ids],
-                                                               })
+            tasks_no_parent = self.task_ids.filtered(lambda task: not task.parent_id)
+            task_template_dict = {}
+            flag = False
+            for i in tasks_no_parent:
+                child_ids = [Command.create({'name' : task.name,
+                                             'user_ids': [Command.link(user.id) for user in task.user_ids],
+                                             'partner_id': task.partner_id.id,
+                                             'project_id': project.id,
+                                             'date_deadline': task.date_deadline,
+                                             'priority': task.priority,
+                                             'description': task.description,}) for task in i.child_ids]
+                task_template = self.env['project.task'].create({"name": i.name,
+                                                  'user_ids': [Command.link(user.id) for user in i.user_ids],
+                                                  'partner_id': i.partner_id.id,
+                                                  'project_id': project.id,
+                                                  'date_deadline': i.date_deadline,
+                                                  'priority': i.priority,
+                                                  'description': i.description,
+                                                  'child_ids': child_ids})
+                for child in range(len(i.child_ids)):
+                    if i.child_ids[child].child_ids:
+                        task_template_dict.update({task_template.child_ids[child] : i.child_ids[child].child_ids})
+                        flag = True
+            sub_dict = task_template_dict
+            while (flag):
+                flag = False
+                task_template_dict = sub_dict
+                sub_dict = {}
+                for rec in range(len(list(task_template_dict.keys()))):
+                        child_templates = [Command.create({'name': task.name,
+                                                   'user_ids': [Command.link(user.id) for user in task.user_ids],
+                                                   'partner_id': task.partner_id.id,
+                                                   'project_id': project.id,
+                                                   'date_deadline': task.date_deadline,
+                                                   'priority': task.priority,
+                                                   'description': task.description, })
+                                   for task in task_template_dict[list(task_template_dict.keys())[rec]]]
+                        list(task_template_dict.keys())[rec].update({'child_ids': child_templates})
+                        for i in task_template_dict[list(task_template_dict.keys())[rec]]:
+                            if i.child_ids:
+                                flag = True
+                                task = list(task_template_dict.keys())[rec].child_ids.filtered(lambda task: task.name == i.name)
+                                sub_dict.update({task : i.child_ids})
         return {
             'type': 'ir.actions.act_window',
             'view_mode': 'form',
